385671fd6c2cb80d13bebba380c86746d8450980
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-09-23 13:51:26 +0100

    Add receiveAttack test

--- __tests__/Gameboard.test.js
+++ __tests__/Gameboard.test.js
@@ -11,7 +11,7 @@ describe("gameboard functions", () => {
   });
 
   test("Generates 10x10 board", () => {
-  let board = [];
+    let board = [];
     for (let i = 0; i < 10; i++) {
       let row = [];
       for (let j = 0; j < 10; j++) {
@@ -23,15 +23,47 @@ describe("gameboard functions", () => {
   });
 
   test("Cannot place ship out of bounds of the columns.", () => {
-    expect(testBoard.isPlacementPossible(testCarrier, [0,7], 'horizontal')).toBe(false);
-  })
+    expect(
+      testBoard.isPlacementPossible(testCarrier, [0, 7], "horizontal")
+    ).toBe(false);
+  });
 
   test("Cannot place ship out of bounds of the rows.", () => {
-    expect(testBoard.isPlacementPossible(testCarrier, [7,0], 'vertical')).toBe(false);
-  })
+    expect(testBoard.isPlacementPossible(testCarrier, [7, 0], "vertical")).toBe(
+      false
+    );
+  });
 
   test("Cannot place ship where ship already exists", () => {
     testBoard.board[0][0].hasShip = true;
-    expect(testBoard.isPlacementPossible(testCarrier, [0,0], 'horizontal')).toBe(false);
-  })
+    expect(
+      testBoard.isPlacementPossible(testCarrier, [0, 0], "horizontal")
+    ).toBe(false);
+  });
+
+  test("Places Ship on all squares if move valid", () => {
+    testBoard.place(testCarrier, [0, 0], "horizontal");
+    for (let i = 0; i < testCarrier.length; i++) {
+      expect(testBoard.board[0][i].hasShip).toBe(true);
+    }
+  });
+
+  test("Doesn't Place Ship on any squares if move is invalid", () => {
+    testBoard.place(testCarrier, [0, 7], "vertical");
+    for (let i = 0; i < testCarrier.length; i++) {
+      expect(testBoard.board[i][0].hasShip).toBe(false);
+    }
+  });
+
+  test("Receives Attack on Empty Square", () => {
+    testBoard.receiveAttack([0,0]);
+    expect(testBoard.board[0][0].isHit).toBe(true);
+  });
+
+  test("Receives Attack on Ship Square, adds 1 to ship hits", () => {
+    testBoard.place(testCarrier, [0, 0], "horizontal");
+    testBoard.receiveAttack([0,0]);
+    expect(testBoard.board[0][0].isHit).toBe(true);
+    expect(testCarrier.hits).toBe(1);
+  });
 });
